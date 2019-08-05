5a45738b79e2fa2481fce98ef054117db3ac0244
junipera <juniper.alcorn@gmail.com>
2019-08-05 10:11:28 -0400

    max binary heap extract max

--- Heaps/maxBinaryHeap.js
+++ Heaps/maxBinaryHeap.js
@@ -9,19 +9,72 @@
 
 class MaxBinaryHeap{
     constructor(){
-        this.values = []
+        this.values = [41, 39, 33, 18, 27, 12]
     }
 
     insert(val){
         this.values.push(val)
         let index = this.values.length - 1
-        let parentIndex = Math.floor((index - 1) / 2)
+        const element = this.values[index]
 
-        while (this.values[parentIndex] < this.values[index]){
-            this.values[parentIndex] = val
-            this.values[index] = this.values[parentIndex]
+        while (index > 0){
+            let parentIndex = Math.floor((index - 1) / 2)
+            let parent = this.values[parentIndex]
+            if(element <= parent) break
+            this.values[parentIndex] = element
+            this.values[index] = parent
 
             index = parentIndex
         }
     }
+    extractMax(){
+        let first = this.values[0]
+        let last = this.values.pop()
+        this.values[0] = last
+
+        let parentIndex = 0
+        let length = this.values.length
+        let element = this.values[0]
+
+        while (true){
+            let leftChildIndex = 2 * parentIndex + 1
+            let rightChildIndex = 2 * parentIndex + 2
+
+            let leftChild, rightChild
+            let swap = null
+
+            if(leftChildIndex < length){
+                leftChild = this.values[leftChildIndex]
+                if(leftChild > element){
+                    swap = leftChildIndex
+                }
+            }
+
+            if(rightChildIndex < length){
+                rightChild = this.values[rightChildIndex]
+                if (
+                    (swap === null && rightChild > element) || 
+                    (swap!== null && rightChild > leftChild)
+                    ){
+                        swap = rightChildIndex
+                }
+            }
+
+            if(swap === null) break
+            this.values[parentIndex] = this.values[swap]
+            this.values[swap] = element
+
+            parentIndex = swap
+        }
+
+        return first;
+    }
 }
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
+console.log(heap.values)
+console.log(heap.extractMax())
+console.log(heap.values)
+console.log(heap.extractMax())
+console.log(heap.values)
